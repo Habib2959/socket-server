74be4a5245e4e1000c7624089d10c6565ded5ef9
Habib2959 <rahman.habibjr59@gmail.com>
2024-03-14 14:44:12 +0600

    cors removed

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import cors from "cors";
 const app = express();
 const httpServer = createServer(app);
 const corsOptions = {
-	origin: ["http://localhost:5173", "https://socket-cllient.vercel.app"],
+	origin: "*",
 };
 app.use(cors(corsOptions));
 const io = new Server(httpServer, {
 	/* options */ cors: {
-		origin: ["http://localhost:5173", "https://socket-cllient.vercel.app"],
+		origin: "*",
 		methods: ["GET", "POST"],
 		credentials: true,
 	},

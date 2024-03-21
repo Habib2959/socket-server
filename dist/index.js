e191507d0e303ed96d283d359bc47bb2326657a3
Habib2959 <rahman.habibjr59@gmail.com>
2024-03-21 12:46:20 +0600

    node server test with post request

--- dist/index.js
+++ dist/index.js
@@ -5,21 +5,34 @@ import cors from "cors";
 var app = express();
 var httpServer = createServer(app);
 var corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "*",
 };
 app.use(cors(corsOptions));
+app.use(express.json());
 var io = new Server(httpServer, {
-    /* options */ cors: {
-        origin: "http://localhost:5173",
+    cors: {
+        origin: corsOptions.origin,
+        // allowedHeaders: ["my-custom-header"],
+        // credentials: true,
     },
+    // allowRequest: (req, callback) => {
+    // 	const noOriginHeader = req.headers.origin === undefined;
+    // 	callback(null, noOriginHeader); // only allow requests without 'origin' header
+    // },
 });
 io.on("connection", function (socket) {
-    socket.on("message", function (msg) {
-        socket.broadcast.emit("received_message", msg);
-    });
+    // io.sockets.emit("this", { will: "hello" });
+    // socket.on("message", (msg) => {
+    // 	socket.broadcast.emit("received_message", msg);
+    // });
 });
 httpServer.listen(3000);
 app.get("/", function (_req, res) {
     res.status(200).json({ message: "Connected to server" });
+});
+app.post("/send", function (req, res) {
+    var message = req.body.message;
+    io.sockets.emit("test001", message);
+    res.status(200).json({ message: "Message sent" });
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,38 @@ import cors from "cors";
 const app = express();
 const httpServer = createServer(app);
 const corsOptions = {
-	origin: ["http://localhost:5173", "https://socket-cllient.vercel.app"],
+	origin: "*",
 };
 app.use(cors(corsOptions));
+app.use(express.json());
 
 const io = new Server(httpServer, {
 	cors: {
 		origin: corsOptions.origin,
-		allowedHeaders: ["my-custom-header"],
-		credentials: true,
+		// allowedHeaders: ["my-custom-header"],
+		// credentials: true,
 	},
-	allowRequest: (req, callback) => {
-		const noOriginHeader = req.headers.origin === undefined;
-		callback(null, noOriginHeader); // only allow requests without 'origin' header
-	},
+	// allowRequest: (req, callback) => {
+	// 	const noOriginHeader = req.headers.origin === undefined;
+	// 	callback(null, noOriginHeader); // only allow requests without 'origin' header
+	// },
 });
 
 io.on("connection", (socket) => {
-	socket.on("message", (msg) => {
-		socket.broadcast.emit("received_message", msg);
-	});
+	// io.sockets.emit("this", { will: "hello" });
+	// socket.on("message", (msg) => {
+	// 	socket.broadcast.emit("received_message", msg);
+	// });
 });
 
 httpServer.listen(3000);
 
 app.get("/", (_req, res) => {
 	res.status(200).json({ message: "Connected to server" });
+});
+
+app.post("/send", (req, res) => {
+	const { message } = req.body;
+	io.sockets.emit("test001", message);
+	res.status(200).json({ message: "Message sent" });
 });
